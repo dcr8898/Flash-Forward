7d6dc1816c4bb9be6e0f2009bd030b3a134a8301
Damian C. Rossney <dcrossney@gmail.com>
2015-10-02 19:46:19 -0400

    Refactor at_variables.

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,13 +1,11 @@
 get '/rounds/:round_id' do
-  round = Round.find_by(id: params[:round_id])
-  all_cards = round.card_ids
-  spent_cards = round.guesses.map { |guess| guess.card_id if guess.answered = true }
-  available_cards = (all_cards - spent_cards)
+  @round = Round.find_by(id: params[:round_id])
+  available_cards = @round.available_cards
   if available_cards.empty?
-    "game summary"
+    erb :'rounds/show'
   else
     next_card_id = available_cards.sample
-    next_guess = Guess.find_or_create_by(round_id: round.id, card_id: next_card_id)
+    next_guess = Guess.find_or_create_by(round_id: @round.id, card_id: next_card_id)
     redirect "/guesses/#{next_guess.id}"
   end
 end
