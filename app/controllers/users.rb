97fda920b22147a03d27e89ee7f276717a19e086
Phil Sofia <phil.sofia@gmail.com>
2015-10-02 19:25:18 -0400

    Create route post /users and add error message to new view if user object save fails

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -6,3 +6,16 @@ get '/users/login' do
   erb :'users/login'
 end
 
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    redirect "/users/#{@user.id}"
+  else
+    @errors = @entry.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+
+  
+
