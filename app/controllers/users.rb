8e0eb557bd3bcb53636acde4df2d2668ecda4cf0
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-03 15:14:39 -0400

    fix login bug

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -2,10 +2,6 @@ get '/users/new' do
   erb :'users/new'
 end
 
-get '/users/login' do
-  erb :'users/login'
-end
-
 post '/users' do
   @user = User.new(params[:user])
   if @user.save
@@ -16,16 +12,33 @@ post '/users' do
   end
 end
 
+get '/users/login' do
+  erb :'users/login'
+end
+
+
 post '/users/login' do
+  # if user = User.find_by(name: params[:user][:name])
+  #   if params[:user][:password_hash] == user.password_hash
+  #     session[:user_id] = user.id
+  #     redirect "/users/#{user.id}"
+  #   else
+  #     @errors = ["Password doesn't match username!"]
+  #     redirect '/users/login'
+  #   end
+  # else
+  #   @errors = ["Username doesn't exist!"]
+  #   redirect '/users/login'
+  # end
   user_params = params[:user]
-  user = User.find_by(username: user_params[:name])
-  if user && user.password == user_params[:password]
-   session[:user_id] = user.id
-   flash[:login] = "Successfully logged in!"
-   redirect '/'
+  user = User.find_by(name: user_params[:name])
+  if user && user.password_hash == user_params[:password_hash]
+    session[:user_id] = user.id
+    flash[:message] = "Successfully logged in."
+    redirect '/'
   else
-   flash[:error] = "Mismatched username/password."
-   redirect '/users/login'
+    flash[:message] = "Cannot log in."
+    redirect '/users/login'
   end
 end
 
