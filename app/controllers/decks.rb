4639fbfb56b6dbe1223af11ab92991fa081b1a98
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-03 17:38:35 -0400

    add splash screen to start the game

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,5 +1,7 @@
 get '/decks/:deck_id' do
+  user = User.find_by(id: session[:user_id])
   @deck = Deck.find_by(id: params[:deck_id])
+  @round = Round.find_or_create_by(user_id: user.id, deck_id: @deck.id)
   @cards = @deck.cards.all
   erb :'/decks/show'
 end
