c9a463c875d013d60a6e422a8596b8ba04e1dd20
Damian C. Rossney <dcrossney@gmail.com>
2015-10-02 21:08:48 -0400

    Evaluate answers

--- app/controllers/guesses.rb
+++ app/controllers/guesses.rb
@@ -3,3 +3,18 @@ get '/guesses/:guess_id' do
 
   erb :'guesses/show'
 end
+
+post '/guesses/:guess_id' do
+  guess = Guess.find_by(id: params[:guess_id])
+  guess.increment(:count)
+  if guess.card.correct?(params[:answer])
+    guess.answered = true
+    "correct"
+  else
+    "incorrect"
+  end
+
+  # redirect "/rounds/#{guess.round_id}"
+end
+
+
