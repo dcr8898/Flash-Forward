c4afc82d8f055b6dc7d021be630ca129e0f7970a
Damian C. Rossney <dcrossney@gmail.com>
2015-10-04 13:58:25 -0400

    Add start round button to /decks/:deck_id page
    Since rounds are now initiated with POST /rounds (with a deck_id parameter),
    I added a button (instead of a link) to the dekcs/show view.
    I also edited the /decks/:deck_id controller route to eliminate unnecessary variables.

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -20,9 +20,7 @@ post '/decks' do
 end
 
 get '/decks/:deck_id' do
-  user = User.find_by(id: session[:user_id])
   @deck = Deck.find_by(id: params[:deck_id])
-  @round = Round.find_or_create_by(user_id: user.id, deck_id: @deck.id)
   @cards = @deck.cards.all
   erb :'/decks/show'
 end
