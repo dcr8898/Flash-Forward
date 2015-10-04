81c240bfbd59a756c62a90e0fed3188e7adbadf9
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-04 12:25:38 -0400

    made post route to create a new card

--- app/controllers/cards.rb
+++ app/controllers/cards.rb
@@ -1,3 +1,11 @@
-get '/cards/new' do
+get '/decks/:deck_id/cards/new' do
+  @deck = Deck.find_by(id: params[:deck_id])
+  @card = Card.new
   erb :'/cards/new'
+end
+
+post '/decks/:deck_id/cards' do
+  deck = Deck.find(params[:deck_id])
+  card = deck.cards.create(params[:card])
+  redirect '/'
 end
