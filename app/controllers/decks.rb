10802fa8115f935add6cc77f940a01de4cfe5d6f
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-03 21:46:29 -0400

    create route for deck creation

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -3,6 +3,10 @@ get '/decks' do
   erb :'index'
 end
 
+get '/decks/new' do
+  
+end
+
 get '/decks/:deck_id' do
   user = User.find_by(id: session[:user_id])
   @deck = Deck.find_by(id: params[:deck_id])
