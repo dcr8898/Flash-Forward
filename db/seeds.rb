1d7871263ff3fe4a890e294b231154a5a3d03438
spacebear5000 <david.giahuy.tu@gmail.com>
2015-10-04 22:44:36 -0400

    add deck to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,6 +77,19 @@ cards = new_cards.map { |card| Card.create(deck_id:  deck.id,
 
 # End of Rebecca's deck
 
+# David's Deck
+new_cards = [["H.P. Lovecraft character also known as an Outer God",  "Nyarlathotep"],
+             ["Son of Shiva and Parvati",                             "Ganeesha"],
+             ["Bird-like creature in Hindu and Buddhist mythology",   "Garuda"],
+             ["A million eyes and mouths",                            "Metatron"],
+             ["Cat that can speak human tongue",                      "Cait Sith"],
+             ["Body of a bull, head of a hog",                        "Catoblepas"],
+             ["Residents of the underworld in Japanese mythology",    "Yomotsu-Shikome"],
+             ["The first to eat the fruit of knowledge",              "Lilith"],
+             ["Shapeshifter; known as White Bone Spirit",             "Baigujing"],
+             ["Two headed dog in Greek mythology",                    "Orthrus"]]
+# End of David's Deck
+
 # Place your new decks above this line!
 names = Array.new(50) { Faker::Name.first_name }.uniq
 names.sample(10).each { |user| User.create(name: user, password: user) }
